cfc05c4e5464c92cc5e0c9f45076740ab5a5d6df
Michael Stramel <m.stramel89@gmail.com>
2017-07-15 15:56:42 -0500

    New: Handle semver engine requirements

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+var semver = require("semver");
+
 module.exports = function(pkg) {
-  var requiredVersion = pkg.engines.node.replace(">=", "");
-  var currentVersion = process.version.replace("v", "");
-  if (currentVersion < requiredVersion) {
+  if (!semver.satisfies(process.version, pkg.engines.node)) {
     console.error(
       "%s requires at least version %s of Node, please upgrade",
       pkg.name,
-      requiredVersion
+      pkg.engines.node
     );
     process.exit(1);
   }
