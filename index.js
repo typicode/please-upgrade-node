ead47dd4df15a9924dcdefa46a2874b892cc5fd0
Michael Stramel <m.stramel89@gmail.com>
2017-07-16 13:57:52 -0500

    Fix documentation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var semver = require("semver");
 module.exports = function(pkg) {
   if (!semver.satisfies(process.version, pkg.engines.node)) {
     console.error(
-      "%s requires at least version %s of Node, please upgrade",
+      "%s requires a Node version that satisfies %s, please upgrade",
       pkg.name,
       pkg.engines.node
     );
