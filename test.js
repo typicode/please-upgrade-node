f7f632c21a33d6daec47a9d465ab4e5b8ed3d7c0
typicode <typicode@gmail.com>
2017-07-19 22:03:18 +0200

    Refactor

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 var assert = require("assert");
+var mock = require('mock-require')
 var pleaseUpgrade = require("./");
 
 Object.defineProperty(process, "version", { value: "v4.0.0" });
 
 var count = 0;
+var name = 'Lorem Ipsum'
 
-process.exit = function(code) {
+process.exit = function (code) {
   count = count + 1;
 };
 
@@ -14,52 +16,69 @@ function countShouldBe(expected) {
 }
 
 // Should not call process.exit
-pleaseUpgrade({
-  name: "PKG_NAME",
+mock('./package.json', {
+  name: name,
   engines: {
     node: ">=1.2.0"
   }
-});
+})
+pleaseUpgrade();
 countShouldBe(0);
 
-pleaseUpgrade({
-  name: "PKG_NAME",
+mock('./package.json', {
+  name: name,
   engines: {
     node: ">=4.0.0"
   }
 });
+pleaseUpgrade();
 countShouldBe(0);
 
-pleaseUpgrade({
-  name: "PKG_NAME",
+mock('./package.json', {
+  name: name,
   engines: {
     node: ">=4"
   }
 });
+pleaseUpgrade();
 countShouldBe(0);
 
 // Should call process.exit
 
-pleaseUpgrade({
-  name: "PKG_NAME",
+mock('./package.json', {
+  name: name,
   engines: {
     node: ">=4.0.1"
   }
 });
+pleaseUpgrade();
 countShouldBe(1);
 
-pleaseUpgrade({
-  name: "PKG_NAME",
+mock('./package.json', {
+  name: name,
   engines: {
     node: ">=6.0.0"
   }
 });
+pleaseUpgrade();
 countShouldBe(2);
 
-pleaseUpgrade({
-  name: "PKG_NAME",
+mock('./package.json', {
+  name: name,
   engines: {
     node: ">=8"
   }
 });
+pleaseUpgrade();
 countShouldBe(3);
+
+// Should support pkgDir
+
+mock('../package.json', {
+  name: name,
+  engines: {
+    node: ">=8"
+  }
+});
+pleaseUpgrade('..');
+countShouldBe(4);
