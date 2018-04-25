dd5687f19ea5dd44c0ab7ee0cba0e1c6de82bd60
typicode <typicode@gmail.com>
2018-04-26 00:00:20 +0200

    Fix bug with version comparison and Node 10

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+var semverCompare = require('semver-compare');
+
 module.exports = function (pkg) {
   var requiredVersion = pkg.engines.node.replace(">=", "");
   var currentVersion = process.version.replace("v", "");
-  if (currentVersion < requiredVersion) {
+  if (semverCompare(currentVersion, requiredVersion) === -1) {
     console.error(
       "%s requires at least version %s of Node, please upgrade",
       pkg.name,
